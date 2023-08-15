1354bed64fb5b435b0cc0bda6980c7c6a32edbb5
CoderFrancis <mfcollegeacc@gmail.com>
2023-08-15 11:22:45 +0530

    20230815 @1120

--- MyDataAnalysisProject/scripts/data_analysis.py
+++ MyDataAnalysisProject/scripts/data_analysis.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
 data = {'name': ['John', 'Jane', 'Sue'], 'age': [28, 34, 45], 'city': ['New York', 'Los Angeles', 'Chicago']}
-df = pd.DataFrame(data)
-print(df)
+df = pd.read_csv('data/data.csv')
+print(df.head())
